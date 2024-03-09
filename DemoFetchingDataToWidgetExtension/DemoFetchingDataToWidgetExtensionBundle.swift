cb70be10a835d22925482ed907ce628d5643ded5
Filip Štěpánek <>
2024-03-09 09:38:53 +0100

    Widget extension added.

---
+++ DemoFetchingDataToWidgetExtension/DemoFetchingDataToWidgetExtensionBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DemoFetchingDataToWidgetExtensionBundle.swift
+//  DemoFetchingDataToWidgetExtension
+//
+//  Created by Filip Štěpánek on 09.03.2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DemoFetchingDataToWidgetExtensionBundle: WidgetBundle {
+    var body: some Widget {
+        DemoFetchingDataToWidgetExtension()
+    }
+}
