67dd2235bee875b1ddef46b169a6724a54274ebe
Filip Štěpánek <>
2024-03-09 09:36:43 +0100

    empty new project - master branch

---
+++ DemoFetchingDataToWidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemoFetchingDataToWidget
+//
+//  Created by Filip Štěpánek on 09.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DemoFetchingDataToWidget/DemoFetchingDataToWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoFetchingDataToWidgetApp.swift
+//  DemoFetchingDataToWidget
+//
+//  Created by Filip Štěpánek on 09.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct DemoFetchingDataToWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
